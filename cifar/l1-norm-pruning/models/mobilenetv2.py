5486af65530f61e6688e542f1f2f13dbebf88e69
Brendan Duke <dukebw@mcmaster.ca>
2018-11-07 14:11:57 -0500

    Support finetuning mobilenetv2.

--- cifar/l1-norm-pruning/models/mobilenetv2.py
+++ cifar/l1-norm-pruning/models/mobilenetv2.py
@@ -3,18 +3,18 @@
 See the paper "Inverted Residuals and Linear Bottlenecks:
 Mobile Networks for Classification, Detection and Segmentation" for more details.
 '''
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Block(nn.Module):
     '''expand + depthwise + pointwise'''
-    def __init__(self, in_planes, out_planes, expansion, stride):
+    def __init__(self, in_planes, out_planes, expansion, stride, planes):
         super(Block, self).__init__()
         self.stride = stride
 
-        planes = expansion * in_planes
+        if planes is None:
+            planes = expansion * in_planes
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, groups=planes, bias=False)
@@ -39,32 +39,42 @@ class Block(nn.Module):
 
 class MobileNetV2(nn.Module):
     # (expansion, out_planes, num_blocks, stride)
-    cfg = [(1, 16, 1, 1),
-           (6, 24, 2, 1),  # NOTE: change stride 2 -> 1 for CIFAR10
-           (6, 32, 3, 2),
-           (6, 64, 4, 2),
-           (6, 96, 3, 1),
-           (6, 160, 3, 2),
-           (6, 320, 1, 1)]
+    base_cfg = [(1, 16, 1, 1),
+                (6, 24, 2, 1),  # NOTE: change stride 2 -> 1 for CIFAR10
+                (6, 32, 3, 2),
+                (6, 64, 4, 2),
+                (6, 96, 3, 1),
+                (6, 160, 3, 2),
+                (6, 320, 1, 1)]
 
     def __init__(self, dataset, depth, cfg=None):
         super(MobileNetV2, self).__init__()
         num_classes = 10
+        self.cfg = cfg
+
         # NOTE: change conv1 stride 2 -> 1 for CIFAR10
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
-        self.layers = self._make_layers(in_planes=32)
+        self.layers = self._make_layers(in_planes=32, cfg=cfg)
         self.conv2 = nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(1280)
         self.linear = nn.Linear(1280, num_classes)
 
-    def _make_layers(self, in_planes):
+    def _make_layers(self, in_planes, cfg):
         layers = []
-        for expansion, out_planes, num_blocks, stride in self.cfg:
+        block_i = 0
+        for expansion, out_planes, num_blocks, stride in self.base_cfg:
             strides = [stride] + [1]*(num_blocks-1)
             for stride in strides:
-                layers.append(Block(in_planes, out_planes, expansion, stride))
+                if cfg is None:
+                    planes = None
+                else:
+                    planes = cfg[block_i]
+
+                layers.append(
+                    Block(in_planes, out_planes, expansion, stride, planes))
                 in_planes = out_planes
+                block_i += 1
         return nn.Sequential(*layers)
 
     def forward(self, x):
